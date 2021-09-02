3b7c8698e27ced1483e8858baff27b496b16eb93
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-09-03 00:51:59 +0100

    fixed text sections and links, copy popup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 const copy = document.getElementById('copy');
 const link = document.getElementById('link');
+const box = document.getElementById('copyBox');
 
 copy.addEventListener('click', () => {
     navigator.clipboard.writeText('shaumikrahman123@gmail.com');
     link.style.color = '#ffed8e';
+    
+    if (box.classList.contains('off')) {
+        box.classList.remove('off');
+        setTimeout(() => {
+            box.classList.add('off');
+        }, 1000);
+    }
 });
